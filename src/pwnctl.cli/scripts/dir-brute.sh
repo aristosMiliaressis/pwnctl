399ca337a700d9db7e7b0785db36da1ea6b47e3b
aristos <aristosmiliaresis@gmail.com>
2022-08-28 20:16:51 +0300

    work in progress

--- src/pwnctl.cli/scripts/dir-brute.sh
+++ src/pwnctl.cli/scripts/dir-brute.sh
@@ -4,8 +4,7 @@ url=$1
 dict=$2
 temp=`mktemp`; 
 
-
-ffuf -o $temp -of json  -H 'User-Agent: Mozilla/5.0' -H 'X-Forwarded-For: 127.0.0.1' -w $dict -u $urlFUZZ
+ffuf -o $temp -of json  -H 'User-Agent: Mozilla/5.0' -H 'X-Forwarded-For: 127.0.0.1' -w $dict -u ${url}FUZZ > /dev/null 2>&1
 
 cat $temp | jq  -r .results[].input.FUZZ | cut -c2- | while read path; do echo "$url${path:1}"; done
 rm $temp
