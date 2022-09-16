7bb916b36eda0d502ef50621b4d911dc05188316
aristos <aristosmiliaresis@gmail.com>
2022-09-16 15:21:16 +0300

    keyword fix

---
+++ src/pwnctl.infra/Persistence/EntityConfiguration/KeywordConfig.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using pwnctl.core.Entities;
+using pwnctl.infra.Persistence.IdGenerators;
+
+namespace pwnctl.infra.Persistence.EntityConfiguration
+{
+    public class KeywordConfig : IEntityTypeConfiguration<Keyword>
+    {
+        public void Configure(EntityTypeBuilder<Keyword> builder)
+        {
+            builder.Property(c => c.Id).HasValueGenerator<HashIdValueGenerator>();
+
+            builder.HasKey(t => t.Id);
+
+            builder.HasIndex(nameof(Keyword.Word)).IsUnique();
+        }
+    }
+}
