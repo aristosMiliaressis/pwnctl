c0e4e4ba85131d8841e2f4e18b7992f56cfc5b7f
aristos <aristosmiliaresis@gmail.com>
2022-09-20 19:31:24 +0300

    ci cache nuget & docker layers

--- src/pwnctl.worker/scripts/notify-status.sh
+++ src/pwnctl.worker/scripts/notify-status.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $(wc -l $PWNCTL_INSTALL_PATH/queue/pending | cut -d ' ' -f 1) -eq 1 ]]
+then 
+    exit
+fi
+
 echo "SELECT Name,Value FROM Tags" | pwnctl query | jq -r 'select( .Name == "foundby" ) | .Value' | sort | uniq -c | sort \
     && echo "SELECT ShortName FROM Tasks JOIN TaskDefinitions ON Tasks.DefinitionId = TaskDefinitions.Id" | pwnctl query | jq .ShortName -r | sort | uniq -c | sort \
     && pwnctl summary && echo \
