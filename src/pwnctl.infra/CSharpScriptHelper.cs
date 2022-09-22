89470ed254f8b8245bafea3ddc9e286df06a5163
aristos <aristosmiliaresis@gmail.com>
2022-09-22 15:04:03 +0300

    single-file publish CSharpScript support fix pt2

--- src/pwnctl.infra/CSharpScriptHelper.cs
+++ src/pwnctl.infra/CSharpScriptHelper.cs
@@ -1,16 +1,96 @@
 using pwnctl.core.BaseClasses;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Scripting;
-using Microsoft.CodeAnalysis.CSharp.Scripting;
 using System.Reflection;
 using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace pwnctl.infra
 {
+    // this is a workaround to use `CSharpScript` with single-file publish
+    // stolen from: https://github.com/andersstorhaug/SingleFileScripting
     public static class CSharpScriptHelper
     {
-        // this is a workaround to use `CSharpScript` with single-file publish
-        // stolen from: https://github.com/andersstorhaug/SingleFileScripting
+        public static bool Evaluate(string script, BaseAsset asset)
+        {
+            var references = new List<MetadataReference>
+            {
+                GetReference(typeof(BaseAsset)),
+                GetReference(typeof(System.Linq.Enumerable)),
+                GetReference(typeof(System.Collections.Generic.IEnumerable<>))
+            };
+
+            Assembly.GetEntryAssembly().GetReferencedAssemblies()
+                .ToList()
+                .ForEach(a => references.Add(MetadataReference.CreateFromFile(Assembly.Load(a).Location)));
+
+            var syntaxTree = SyntaxFactory.ParseSyntaxTree(
+                $"{asset.GetType().Name} => " + script,
+                new CSharpParseOptions(
+                    kind: SourceCodeKind.Script,
+                    languageVersion: LanguageVersion.Latest));
+
+            var funcType = typeof(Func<,>).MakeGenericType(asset.GetType(), typeof(bool));
+
+            var compilation = CSharpCompilation.CreateScriptCompilation(
+                assemblyName: "Script",
+                syntaxTree,
+                references,
+                returnType: funcType);
+
+            var delegateType = typeof(Func<,>).MakeGenericType(typeof(object[]), typeof(Task<>).MakeGenericType(funcType));
+            object submissionFactory = null;
+
+            using (var peStream = new MemoryStream())
+            using (var pdbStream = new MemoryStream())
+            {
+                // https://github.com/dotnet/roslyn/blob/version-3.2.0/src/Scripting/Core/ScriptBuilder.cs#L121
+                // https://github.com/dotnet/roslyn/blob/version-3.2.0/src/Scripting/Core/Utilities/PdbHelpers.cs#L10
+                var result = compilation.Emit(
+                    peStream,
+                    pdbStream,
+                    xmlDocumentationStream: null,
+                    win32Resources: null,
+                    manifestResources: null,
+                    new EmitOptions(
+                        debugInformationFormat: DebugInformationFormat.PortablePdb,
+                        pdbChecksumAlgorithm: default(HashAlgorithmName)));
+
+                if (!result.Success)
+                {
+                    throw new Exception($"Compilation of CScript '{script}' failed");
+                }
+
+                var scriptAssembly = AppDomain.CurrentDomain.Load(peStream.ToArray(), pdbStream.ToArray());
+
+                // https://github.com/dotnet/roslyn/blob/version-3.2.0/src/Scripting/Core/ScriptBuilder.cs#L188
+                var entryPoint = compilation.GetEntryPoint(CancellationToken.None) ?? throw new InvalidOperationException("Entry point could be determined");
+
+                var entryPointType = scriptAssembly
+                    .GetType(
+                        $"{entryPoint.ContainingNamespace.MetadataName}.{entryPoint.ContainingType.MetadataName}",
+                        throwOnError: true,
+                        ignoreCase: false);
+
+                var entryPointMethod = entryPointType?.GetTypeInfo().GetDeclaredMethod(entryPoint.MetadataName) ?? throw new InvalidOperationException("Entry point method could be determined");
+
+                var createDelegateMethod = typeof(MethodInfo)
+                                                .GetMethod(nameof(MethodInfo.CreateDelegate), new Type[] { })
+                                                .MakeGenericMethod(delegateType);
+                submissionFactory = createDelegateMethod.Invoke(entryPointMethod, new object[] { });
+            }
+
+            var delegateInvokeMethod = delegateType.GetMethod(nameof(Func<BaseAsset>.Invoke));
+
+            var evalTask = delegateInvokeMethod.Invoke(submissionFactory, new object[] { new object[] { null, null } });
+
+            var filter = typeof(Task<>).MakeGenericType(funcType).GetProperty(nameof(Task<BaseAsset>.Result)).GetValue(evalTask);
+            var invokeMethod = funcType.GetMethod(nameof(Func<BaseAsset>.Invoke));
+            return (bool)invokeMethod.Invoke(filter, new object[] { asset });
+        }
+
+        // https://github.com/dotnet/runtime/issues/36590#issuecomment-689883856
         private static MetadataReference GetReference(Type type)
         {
             unsafe
@@ -21,39 +101,6 @@ namespace pwnctl.infra
                         .GetReference()
                     : throw new InvalidOperationException($"Could not get raw metadata for type {type}");
             }
-        }
-
-
-        public static bool Evaluate(string script, BaseAsset asset)
-        {
-            // var options = ScriptOptions.Default.AddReferences(typeof(BaseAsset).Assembly)
-            //                                     .AddReferences(Assembly.GetAssembly(typeof(System.Linq.Enumerable)))
-            //                                     .AddReferences(Assembly.GetAssembly(typeof(System.Collections.Generic.IEnumerable<>)));
-
-
-            var references = new[]
-            {
-                GetReference(typeof(BaseAsset)),
-                GetReference(typeof(System.Linq.Enumerable)),
-                GetReference(typeof(System.Collections.Generic.IEnumerable<>))
-            };
-
-            var options = ScriptOptions.Default.AddReferences(references);
-
-            var funcType = typeof(Func<,>).MakeGenericType(asset.GetType(), typeof(bool));
-            var evalMethod = typeof(CSharpScript).GetMethods(BindingFlags.Public | BindingFlags.Static)
-                                                .First(m => m.Name == nameof(CSharpScript.EvaluateAsync)
-                                                         && m.IsGenericMethod == true)
-                                                .MakeGenericMethod(funcType);
-            var evalTask = evalMethod.Invoke(null, new object[]
-            {
-                    $"{asset.GetType().Name} => " + script,
-                    options, null, null, CancellationToken.None
-            });
-
-            var filter = typeof(Task<>).MakeGenericType(funcType).GetProperty(nameof(Task<BaseAsset>.Result)).GetValue(evalTask);
-            var invokeMethod = funcType.GetMethod(nameof(Func<BaseAsset>.Invoke));
-            return (bool)invokeMethod.Invoke(filter, new object[] { asset });
         }
     }
 }
