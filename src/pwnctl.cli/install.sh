cb8950c5a3503c80e479a88267f9a97264d62ec6
aristos <aristosmiliaresis@gmail.com>
2022-09-23 13:25:34 +0300

    install script fix

--- src/pwnctl.cli/install.sh
+++ src/pwnctl.cli/install.sh
@@ -5,15 +5,18 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-curl https://api.github.com/repos/aristosMiliaressis/pwntainer/releases/latest \
+curl https://api.github.com/repos/aristosMiliaressis/pwntainer/releases/latest 2>/dev/null \
     | jq -r .assets[].browser_download_url \
-    | wget -qi - 
+    | wget -qi -
 
 tar -xzf pwnctl-linux64.tar.gz -C /opt/
 rm pwnctl-linux64.tar.gz
 
 chmod +x /opt/pwnctl/pwnctl
-ln -s /opt/pwnctl/pwnctl /usr/loca/bin/pwnctl
+ln -s /opt/pwnctl/pwnctl /usr/local/bin/pwnctl 2>/dev/null
 
-mkdir /etc/pwnctl
+mkdir /etc/pwnctl 2>/dev/null
 mv /opt/pwnctl/config.ini /etc/pwnctl
+
+echo "All done!"
+echo "Remember to setup your connection string in /etc/pwnctl/config.ini"
