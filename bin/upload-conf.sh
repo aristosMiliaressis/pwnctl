5f80726e524d5c41b095d9aea66beca5567499cd
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2023-01-03 15:45:15 +0200

    git restore scripts

---
+++ bin/upload-conf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+functionUrl=$(aws ssm get-parameter --name /pwnctl/Api/BaseUrl | jq -r .Parameter.Value)
+
+uploadDirectory() {
+     srcDir=$1
+     dstDir=""
+     if [ $# -gt 1 ]; 
+     then 
+          dstDir=$2; 
+          echo "Creating $dstDir"
+          python3 -m awscurl -X PUT --service lambda ${functionUrl}fs/create?path=$dstDir 2>/dev/null
+     fi
+
+     for file in $srcDir/*; 
+     do 
+          if [ -f $file ];
+          then
+               echo "Uploading $file"
+               python3 -m awscurl -X PUT ${functionUrl}fs/upload?path=$dstDir${file#"$srcDir"} \
+                    --service lambda -d @$file \
+                    -H 'Content-Type: text/plain'
+          fi          
+     done
+}
+
+./src/core/pwnctl.svc/scripts/get-psl.sh deployment/
+
+uploadDirectory ./deployment
+uploadDirectory ./src/core/pwnctl.infra/Persistence/seed /seed
