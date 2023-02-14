2cb454c1e3583cab898a5174dfe9732ffb886546
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2023-02-14 15:02:51 +0200

    email-finder & reverse-whois on email tasks

---
+++ src/core/pwnctl.svc/scripts/email-finder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+emailfinder -d $1 | grep 'Total emails' -A 1000 | tail -n +3
---
+++ src/core/pwnctl.svc/scripts/reverse-whois.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+email=$1
+
+curl -s https://viewdns.info/reversewhois/\?q\=$email \
+	-H 'user-agent: Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
+	| htmlq  table#null tr \
+	| tail -n +3 \
+	| htmlq -t td \
+	| grep -P '[\w-]+\.[\.\w-]+'
