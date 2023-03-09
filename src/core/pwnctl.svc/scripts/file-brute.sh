fc304d2c88d719e767c7adeca6de07f5085ac2fc
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2023-03-09 17:26:59 +0200

    http fuzzing task improvements

--- src/core/pwnctl.svc/scripts/file-brute.sh
+++ src/core/pwnctl.svc/scripts/file-brute.sh
@@ -4,7 +4,7 @@ url=$1
 dict=$2
 temp=`mktemp`
 
-ffuf -t 20 -s -o $temp -of json -fc 200 -mc 200,206,401 -w $dict -u ${url}FUZZ &>/dev/null
+ffuf -s -ac -se -r -o $temp -of json -mc 200,206,401 -H "User-Agent: $(uagen)" -w $dict -u ${url}FUZZ &>/dev/null
 
 cat $temp \
     | jq -c '.results[] | {asset: ("%%BASE_URL%%"+.input.FUZZ), tags:{status:.status,location:.redirectlocation}}' \
