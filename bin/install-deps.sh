62c78382624d2ad4a140f2a1aa3c2a6322dde0df
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2023-04-21 16:00:51 +0300

    cleanup

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y git curl wget jq docker nodejs
+sudo apt-get install -y git curl wget jq docker
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
 unzip awscliv2.zip
@@ -6,12 +6,9 @@ rm awscliv2.zip
 ./aws/install --update
 rm -rf ./aws
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash - 
-
-npm install -g aws-cdk
-
 pip install awscurl
 
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -Channel 6.0 -Runtime aspnetcore -InstallDir /usr/share/dotnet --architecture x64
+curl -sSL https://dot.net/v1/dotnet-install.sh \
+    | bash /dev/stdin -Channel 6.0 -Runtime aspnetcore -InstallDir /usr/share/dotnet --architecture x64
 ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
 
