a01534d9ee5e8eddf40371bbb5f19f9d943c4acf
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2023-05-24 19:55:52 +0300

    ping-sweep ipv6 support

--- src/core/pwnctl.svc/scripts/ping-sweep.sh
+++ src/core/pwnctl.svc/scripts/ping-sweep.sh
@@ -2,7 +2,11 @@
 
 cidr=$1
 
-nmap -n -sn -PO -PE $cidr -vv \
+# if it contains ':' collons it is ipv6 so add -6 param
+params=()
+[[ $cidr =~ .*":".* ]] && params+=(-6)
+
+nmap -n -sn -PO -PE $cidr -vv "${params[@]}" 2>/dev/null \
     | grep 'Host is up' -B 1 \
     | tr '\n' ' ' \
     | sed 's/Nmap scan report/\nNmap scan report/g' \
