2b130c9e6e0a07e3c74bb31221f227e9e3c51886
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2023-05-28 13:57:47 +0300

    [Perf] merged waybackurls & gau in one task & added urless

--- src/core/pwnctl.svc/scripts/get-all-urls.sh
+++ src/core/pwnctl.svc/scripts/get-all-urls.sh
@@ -2,4 +2,13 @@
 
 domain=$1
 
-echo $domain | gau --subs --threads 40 --timeout 20 --blacklist png,jpg,jpeg,gif,ico,svg,ttf,woff,woff2,eot,css,pdf
+temp=`mktemp`
+trap "rm $temp" EXIT
+
+echo $domain \
+    | gau --subs --threads 40 --timeout 20 --blacklist png,jpg,jpeg,gif,ico,svg,ttf,woff,woff2,eot,css,pdf > $temp
+
+echo $url | waybackurls \
+    | grep -vE '\.(jpg|png|svg|jpeg|gif|ico|ttf|woff|woff2|eot|css|pdf)\b' >> $temp
+
+cat $temp | urless
--- src/core/pwnctl.svc/scripts/waybackurls.sh
+++ src/core/pwnctl.svc/scripts/waybackurls.sh
@@ -3,4 +3,5 @@
 url=$1
 
 echo $url | waybackurls \
-    | grep -vE '\.(jpg|png|svg|jpeg|gif|ico|ttf|woff|woff2|eot|css|pdf)\b'
+    | grep -vE '\.(jpg|png|svg|jpeg|gif|ico|ttf|woff|woff2|eot|css|pdf)\b' \
+    | urless
