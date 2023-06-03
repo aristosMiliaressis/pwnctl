8f4eb5a4e4ca11d3eb94f923693ea9f4444144a2
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2023-06-03 19:12:25 +0300

    screenshots

--- src/core/pwnctl.svc/scripts/screenshot.sh
+++ src/core/pwnctl.svc/scripts/screenshot.sh
@@ -4,5 +4,4 @@ url=$1
 
 mkdir -p /mnt/efs/screenshots 2>/dev/null
 
-echo $url \
-    | aquatone -silent -chrome-path /usr/bin/chromium -http-timeout 30000 -out /mnt/efs/screenshots
+echo $url | httpx -silent -ss -srd /mnt/efs/screenshots
