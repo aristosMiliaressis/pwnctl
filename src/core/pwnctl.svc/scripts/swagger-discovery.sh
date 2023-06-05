b6ccb4b4f0d7c8bf0ff57e4f8636ca28df67cba3
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2023-06-05 14:04:21 +0300

    swagger_discovery tasks

---
+++ src/core/pwnctl.svc/scripts/swagger-discovery.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+url=$1
+
+nuclei -silent -nc -t /opt/nuclei_templates/discovery/swagger-ui.yaml -u $url \
+    | grep '^\[swagger-ui\]' \
+    | awk '{print $NF}' \
+    | jq -c --raw-input '{Asset:(.), Tags:{"swagger-ui":"true"}}'
