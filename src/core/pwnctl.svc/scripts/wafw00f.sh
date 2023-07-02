21d26f1b40652dad42ef3f4e2bc86a765eb58965
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2023-07-02 15:53:18 +0300

    wafw00f task fixed

--- src/core/pwnctl.svc/scripts/wafw00f.sh
+++ src/core/pwnctl.svc/scripts/wafw00f.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 url=$1
+domain=$(echo $url | unfurl domains)
 temp="`mktemp`.json"
 
 wafw00f -o $temp $url &> /dev/null
 waf=$(cat $temp | jq .[].firewall | sed 's/"None"/null/')
+[ -z $waf ] && waf="null"
 
-cdncheck -silent -j -resp -i pentesterlab.com | jq -c "{asset:\"$url\", tags:{cloud:.cloud_name,cdn:.cdn_name,waf:$waf}}"
+cdncheck -silent -j -resp -i $domain | jq -c "{asset:\"$url\", tags:{cloud:.cloud_name,cdn:.cdn_name,waf:$waf}}"
 
 rm $temp 2>/dev/null
