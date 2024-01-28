2e249fceb8f79c9e42fd493b86df0b0de7494bf0
Aristos Miliaressis <aristosmiliaresis@gmail.com>
2024-01-28 10:38:42 +0200

    build & push docker images in terraform

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -2,6 +2,3 @@
 
 export AWS_REGION=$(aws configure get region)
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-export PWNCTL_PROC_TAG=$(docker images | grep pwnctl-proc | head -n 1 | awk '{print $2}')
-export PWNCTL_EXEC_SHORT_TAG=$(docker images | grep pwnctl-exec-short | head -n 1 | awk '{print $2}')
-export PWNCTL_EXEC_LONG_TAG=$(docker images | grep pwnctl-exec-long | head -n 1 | awk '{print $2}')
--- infra/ecr.tf
+++ infra/ecr.tf
@@ -10,29 +10,79 @@ data "aws_ecr_repository" "proc" {
   name = "pwnctl-proc"
 }
 
-data "external" "proc-latest-tag" {
-  program = ["bash", "-c", "docker images | grep ${data.aws_ecr_repository.proc.repository_url} | head -n 1 | awk '{print $2}'| jq --raw-input '. | { tag: (.) }'"]
-}
-
-data "external" "exec-short-latest-tag" {
-  program = ["bash", "-c", "docker images | grep ${data.aws_ecr_repository.proc.repository_url} | head -n 1 | awk '{print $2}'| jq --raw-input '. | { tag: (.) }'"]
-}
-
-data "external" "exec-long-latest-tag" {
-  program = ["bash", "-c", "docker images | grep ${data.aws_ecr_repository.proc.repository_url} | head -n 1 | awk '{print $2}'| jq --raw-input '. | { tag: (.) }'"]
+data "external" "commit_hash" {
+  program = ["bash", "-c", "git rev-parse --short HEAD | jq --raw-input '. | { sha: (.) }'"]
 }
 
 resource "docker_registry_image" "exec_short" {
-  name          = "${data.aws_ecr_repository.exec_short.repository_url}:${data.external.exec-short-latest-tag.result.tag}"
+  name          = "${data.aws_ecr_repository.exec_short.repository_url}:${data.external.commit_hash.result.sha}"
   keep_remotely = true
 }
 
 resource "docker_registry_image" "exec_long" {
-  name          = "${data.aws_ecr_repository.exec_long.repository_url}:${data.external.exec-long-latest-tag.result.tag}"
+  name          = "${data.aws_ecr_repository.exec_long.repository_url}:${data.external.commit_hash.result.sha}"
   keep_remotely = true
 }
 
 resource "docker_registry_image" "proc" {
-  name          = "${data.aws_ecr_repository.proc.repository_url}:${data.external.proc-latest-tag.result.tag}"
+  name          = "${data.aws_ecr_repository.proc.repository_url}:${data.external.commit_hash.result.sha}"
   keep_remotely = true
+}
+
+locals {
+  proc_img_src_path   = "${path.module}/../src/core"
+  exec_short_img_src_path   = "${path.module}/../src/core"
+  exec_long_img_src_path   = "${path.module}/../src/core"
+  
+  proc_img_src_sha256 = sha256(join("", [for f in fileset(".", "${local.proc_img_src_path}/**") : filebase64(f)]))
+  exec_short_img_src_sha256 = sha256(join("", [for f in fileset(".", "${local.exec_short_img_src_path}/**") : filebase64(f)]))
+  exec_long_img_src_sha256 = sha256(join("", [for f in fileset(".", "${local.exec_long_img_src_path}/**") : filebase64(f)]))
+
+  proc_build_cmd = <<-EOT
+    docker build -t ${data.aws_ecr_repository.proc.repository_url}:${data.external.commit_hash.result.sha} \
+          --build-arg COMMIT_HASH=${data.external.commit_hash.result.sha} \
+          -f ${local.proc_img_src_path}/pwnctl.proc/Dockerfile ${local.proc_img_src_path}
+  EOT
+
+  exec_short_build_cmd = <<-EOT
+    docker build -t ${data.aws_ecr_repository.exec_short.repository_url}:${data.external.commit_hash.result.sha} \
+          --build-arg COMMIT_HASH=${data.external.commit_hash.result.sha} --ssh default=$SSH_AUTH_SOCK \
+          -f ${local.exec_short_img_src_path}/pwnctl.exec/shortlived/Dockerfile ${local.exec_short_img_src_path}
+  EOT
+
+  exec_long_build_cmd = <<-EOT
+    docker build -t ${data.aws_ecr_repository.exec_long.repository_url}:${data.external.commit_hash.result.sha} \
+          --build-arg COMMIT_HASH=${data.external.commit_hash.result.sha} --ssh default=$SSH_AUTH_SOCK \
+          -f ${local.exec_long_img_src_path}/pwnctl.exec/longlived/Dockerfile ${local.exec_long_img_src_path}
+  EOT
+}
+
+resource "null_resource" "build_push_proc_img" {
+  triggers = {
+    detect_docker_source_changes = local.proc_img_src_sha256
+  }
+
+  provisioner "local-exec" {
+    command = local.proc_build_cmd
+  }
+}
+
+resource "null_resource" "build_push_exec_short_img" {
+  triggers = {
+    detect_docker_source_changes = local.exec_short_img_src_sha256
+  }
+
+  provisioner "local-exec" {
+    command = local.exec_short_build_cmd
+  }
+}
+
+resource "null_resource" "build_push_exec_long_img" {
+  triggers = {
+    detect_docker_source_changes = local.exec_long_img_src_sha256
+  }
+
+  provisioner "local-exec" {
+    command = local.exec_long_build_cmd
+  }
 }
